8297ca428b6015fb29d469776307e7cfb49d00c4
xgxtkkn <karsten.knappe.extern@fiduciagad.de>
2018-11-22 10:22:27 +0100

    applies vue/recommended lint fixes

--- wallaby.js
+++ wallaby.js
@@ -1,31 +1,30 @@
 module.exports = wallaby => {
-    process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
-  
-    return {
-      files: ['src/**/*', 'jest.config.js', 'package.json', 'tsconfig.json', 'babel.config.js'],
-  
-      tests: ['tests/**/*.spec.ts'],
-  
-      env: {
-        type: 'node',
-        runner: 'node',
-      },
-  
-      preprocessors: {
-        '**/*.js?(x)': file => require('@babel/core').transform(
-          file.content,
-          {sourceMap: true, compact: false, filename: file.path})
-      },
-  
-      setup(wallaby) {
-        const jestConfig = require('./package').jest || require('./jest.config')
-        delete jestConfig.transform['^.+\\.tsx?$']
-        wallaby.testFramework.configure(jestConfig)
-      },
-  
-      testFramework: 'jest',
-  
-      debug: true
-    }
+  process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true
+
+  return {
+    files: ['src/**/*', 'jest.config.js', 'package.json', 'tsconfig.json', 'babel.config.js'],
+
+    tests: ['tests/**/*.spec.ts'],
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    preprocessors: {
+      '**/*.js?(x)': file => require('@babel/core').transform(
+        file.content,
+        { sourceMap: true, compact: false, filename: file.path })
+    },
+
+    setup (wallaby) {
+      const jestConfig = require('./package').jest || require('./jest.config')
+      delete jestConfig.transform['^.+\\.tsx?$']
+      wallaby.testFramework.configure(jestConfig)
+    },
+
+    testFramework: 'jest',
+
+    debug: true
   }
-  
+}
